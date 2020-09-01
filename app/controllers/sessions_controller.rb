253611c72aff2760c33f67e4072ba93a3a1996f2
vagrant <vagrant@localhost.localdomain>
2020-09-01 12:05:50 +0000

    start

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if @user.authenticate(session_params[:password])
+      sign_in(@user)
+      redirect_to root_path
+    else
+      flash.now[:danger] = t('.flash.invalid_password')
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to login_path
+  end
+
+  private
+
+    def set_user
+      @user = User.find_by!(mail: session_params[:mail])
+    rescue
+      flash.now[:danger] = t('.flash.invalid_mail')
+      render action: 'new'
+    end
+
+    # 許可するパラメータ
+    def session_params
+      params.require(:session).permit(:mail, :password)
+    end
+end
